ba74fa6950ac850fd846131d5b32f54fcc1d292b
Eldin <eldinhelja@live.com>
2022-06-05 03:56:03 +0200

    Moved method implementations to a .cpp file

--- infothy/src/Information/dmccapacity.cpp
+++ infothy/src/Information/dmccapacity.cpp
@@ -1,3 +1,72 @@
 #include "dmccapacity.h"
 
 using namespace tpp;
+
+double tpp::DMCChannelCapacity::calc_Qkj_fract(size_t j, size_t k) {
+	double sum = 0.0;
+	for (size_t jj = 0; jj < _Q.size(); ++jj) {
+		sum += _p[jj] * _Q[jj][k];
+	}
+	return _Q[j][k] / sum;
+}
+
+double tpp::DMCChannelCapacity::calc_cj_sum(size_t j) {
+	double result = 0.0;
+	for (size_t k = 0; k < _Q.sizeCols(); ++k) {
+		result += TC(_Q[j][k], calc_Qkj_fract(j, k));
+	}
+	return result;
+}
+
+void tpp::DMCChannelCapacity::calcCapVec() {
+	for (size_t j = 0; j < _c.size(); ++j) {
+		_c[j] = std::exp(calc_cj_sum(j));
+	}
+}
+
+double tpp::DMCChannelCapacity::calc_I_L() {
+	double sum = 0.0;
+	for (size_t j = 0; j < _Q.size(); ++j)
+		sum += _p[j] * _c[j];
+	return myLog(sum, TC.getUnit());
+}
+
+void tpp::DMCChannelCapacity::update_px() {
+	double sum;
+	for (size_t j = 0; j < _Q.size(); ++j) {
+		sum = 0.0;
+
+		for (size_t x = 0; x < _Q.size(); ++x)
+			sum += _p[x] * _c[x];
+
+		_p[j] *= _c[j] / sum;
+	}
+}
+
+void tpp::DMCChannelCapacity::computeCapacity() {
+	_c = Vec<double>(_Q.size());
+	setSrcDistrUniform();
+	double I_L = 0.0;
+	for (size_t i = 0; i < _baConfig.maxIterations; ++i) {
+		calcCapVec();
+
+		if (calc_I_U() - (I_L = calc_I_L()) < _baConfig.epsilon) {
+			_capacity = I_L / std::log(unitToBase(Unit::BITS));
+			return;
+		}
+
+		update_px();
+
+	}
+}
+
+void tpp::DMCChannelCapacity::setTransitionMatrix(const DistributionMatrix& Q, const Vec<double>& px) {
+	if (px.size() != Q.size())
+		throw std::logic_error("Source probability distribution vector size not equal to amount of rows in transition matrix.");
+
+	_p = px;
+	_capacity = -1.0;
+	_Q = Q;
+
+	computeSourceInformationSpeed();
+}
--- infothy/src/Information/dmccapacity.h
+++ infothy/src/Information/dmccapacity.h
@@ -58,35 +58,14 @@ namespace tpp {
 		// ----------------------------------
 		
 		// right most term in c_j
-		double calc_Qkj_fract(size_t j, size_t k) {
-			double sum = 0.0;
-			for (size_t jj = 0; jj < _Q.size(); ++jj) {
-				sum += _p[jj] * _Q[jj][k];
-			}
-			return _Q[j][k] / sum;
-		}
+		double calc_Qkj_fract(size_t j, size_t k);
 
-		double calc_cj_sum(size_t j) {
-			double result = 0.0;
-			for (size_t k = 0; k < _Q.sizeCols(); ++k) {				
-				result += TC(_Q[j][k], calc_Qkj_fract(j, k));
-			}
-			return result;
-		}
+		double calc_cj_sum(size_t j);
 
 		// Computes and updates capacity vector
-		void calcCapVec() {
-			for (size_t j = 0; j < _c.size(); ++j) {
-				_c[j] = std::exp(calc_cj_sum(j));
-			}
-		}
+		void calcCapVec();
 
-		double calc_I_L() {
-			double sum = 0.0;
-			for (size_t j = 0; j < _Q.size(); ++j)
-				sum += _p[j] * _c[j];
-			return myLog(sum, TC.getUnit());
-		}
+		double calc_I_L();
 
 		double calc_I_U() {
 			return myLog(*std::max_element(_c.begin(), _c.end()), TC.getUnit());
@@ -94,34 +73,9 @@ namespace tpp {
 
 		// Update source probability distribution
 		// to prepare it for the next iteration in the algorithm
-		void update_px() {
-			double sum;
-			for (size_t j = 0; j < _Q.size(); ++j) {
-				sum = 0.0;
+		void update_px();
 
-				for (size_t x = 0; x < _Q.size(); ++x) 
-					sum += _p[x] * _c[x];
-				
-				_p[j] *= _c[j] / sum;
-			}
-		}
-
-		void computeCapacity() {
-			_c = Vec<double>(_Q.size());
-			setSrcDistrUniform();
-			double I_L = 0.0;
-			for (size_t i = 0; i < _baConfig.maxIterations; ++i) {
-				calcCapVec();
-				
-				if (calc_I_U() - (I_L = calc_I_L()) < _baConfig.epsilon) {
-					_capacity = I_L / std::log(unitToBase(Unit::BITS));
-					return;
-				}
-
-				update_px();
-
-			}
-		}
+		void computeCapacity();
 
 		// ----------------------------------
 
@@ -134,17 +88,7 @@ namespace tpp {
 			setTransitionMatrix(Q, px);
 		}
 
-		void setTransitionMatrix(const DistributionMatrix& Q, const Vec<double>& px) {
-
-			if (px.size() != Q.size())
-				throw std::logic_error("Source probability distribution vector size not equal to amount of rows in transition matrix.");
-			
-			_p = px;
-			_capacity = -1.0;
-			_Q = Q;
-			
-			computeSourceInformationSpeed();
-		}
+		void setTransitionMatrix(const DistributionMatrix& Q, const Vec<double>& px);
 
 		const DistributionMatrix& getTransitionMatrix() const {
 			return _Q;
