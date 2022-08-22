11ea697e7c6fdbf1db0fc649ac69f421e65cc001
Eldin <eldinhelja@live.com>
2022-08-23 00:37:40 +0200

    Basic window display with Raylib 4.2

--- infothy/src/main.cpp
+++ infothy/src/main.cpp
@@ -1,79 +1,19 @@
 #include <iostream>
-#include <string>
-#include <stack>
-#include "DMCSolver.h"
-#include "Tests/channeltest.h"
-#include "Tests/solvertest.h"
-#include "Tests/SrcCodingTest.h"
-#include "UI/MainMenu.h"
+#include "raylib/raylib.h"
 
-void runTests() {
-	tpp::test1();
-	tpp::test2();
-	tpp::test3();
-	tpp::test4();
-	tpp::test5();
-	tpp::test6();
+int main(void)
+{
+    InitWindow(800, 450, "raylib [core] example - basic window");
 
-	tpp::testSolver1();
-	tpp::testSolver2();
-	tpp::testSolver3();
-	tpp::testSolver4();
-}
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
 
-void runCodingTests() {
-	tpp::runSourceCoderTests();
-}
+    CloseWindow();
 
-int main(int argc, char* argv[]) {
-
-	if (argc < 2) {
-		// Arguments were expected
-		// return 1;
-	}
-	
-	// Improve performance, since we arne't using C-style I/O (printf, scanf)
-	std::ios_base::sync_with_stdio(false);
-
-	std::cout << "infothy v0.1" << std::endl;
-	std::cout << "=======================\n\n";
-
-	std::stack<std::unique_ptr<tpp::BaseMenu>> menus;
-	menus.push(std::make_unique<tpp::MainMenu>());
-
-	bool shouldQuit = false;
-	bool isGoBack = false;
-
-	while (!shouldQuit && !menus.empty()) {
-
-		menus.top()->printText();
-
-		int choice = 0;
-
-		if (std::cin >> choice) {
-			
-			try {
-				auto newMenu = menus.top()->getNextMenu(choice, shouldQuit, isGoBack);
-
-				if (isGoBack) {
-					menus.pop();
-					isGoBack = false;
-				}
-				else if (newMenu != nullptr)
-					menus.push(std::move(newMenu));
-			}
-			catch (std::exception& ex) {
-				std::cout << "There was an error: " << ex.what() << std::endl;
-			}
-		}
-		else {
-			std::cin.clear();
-			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		}
-
-		std::cout << std::endl;
-	}
-
-
-	return 0;
+    return 0;
 }
